8e0d2bd90d03ecd645d85e989a4b17e1461a77a7
vimavura <vimavura@my.bridgeport.edu>
2024-11-13 15:52:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('How is your day going? Congratulations on your Node Hello World Application built on Docker!!!!');
+  res.send('This image is from Docker and edited in Github');
 })
 
 app.listen(port, host);
